b305404a8de77a8053975df4556e0068c710755b
EC2 Default User <ec2-user@ip-172-31-8-86.eu-north-1.compute.internal>
2020-07-28 07:31:18 +0000

    sadf

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 cd ~/sh-report-archive
 ./scrape.py
 ./compare.py
-cp current.csv debug-intended-history/$(date +%Y.%m.%d-%H:%M:%S)
+cp current.csv debug-intended-history/$(date +%Y.%m.%d-%H:%M:%S).csv
 mv current.csv last.csv
 git add .
 git commit -m "sadf"
