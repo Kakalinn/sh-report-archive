e41650bb80ed793b69d72e58a8210eb3f7708d93
Bergur Snorrason <bsnorrason@gmail.com>
2020-07-29 10:19:47 +0000

    fdsa

--- compare.py
+++ compare.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # Read the last scrape and compare it to the current.
 # All lines in last.csv, but not in current.csv are appended to archive.csv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug", help = "Doesn't print to archive.csv, prints to stdout instead.", action = "store_true")
+args = parser.parse_args()
+
 fcur = open("current.csv", "r")
 flast = open("last.csv", "r")
 farch = open("archive.csv", "a")
@@ -9,7 +15,9 @@ cur = [ll.rstrip('\n') for ll in fcur]
 last = [ll.rstrip('\n') for ll in flast]
 
 for e in last[::-1]:
-    if e not in cur and e[1] == 'C': farch.write(e + '\n')
+    if e not in cur and e[1] == 'C':
+        if args.d: print(e)
+        else: farch.write(e + '\n')
 
 fcur.close()
 flast.close()
