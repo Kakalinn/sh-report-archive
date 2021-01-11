277fba46e03622c0a967b59bdc06ddfda81791c0
Bergur <bsnorrason@gmail.com>
2021-01-11 10:31:18 +0000

    Automated commit by the run.sh script

--- run.sh
+++ run.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
-echo 1 > test.txt
 ./scrape.py
-echo 2 > test.txt
 ./compare.py
-echo 3 > test.txt
 cp current.csv debug-intended-history/$(date +%Y.%m.%d-%H:%M:%S).csv
-echo 4 > test.txt
 mv current.csv last.csv
-echo 5 > test.txt
 git add .
-echo 6 > test.txt
 git commit -m "Automated commit by the run.sh script"
-echo 7 > test.txt
 git push
-echo 8 > test.txt
 
