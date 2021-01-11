fd2bdc361a64efcbe5c1cd294188d6aa582e8fb0
Bergur <bsnorrason@gmail.com>
2021-01-11 11:30:03 +0000

    Automated commit by the run.sh script at 2021.01.11-11:30:03

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ cd /home/kakali/programming/sh-report-archive
 cp current.csv debug-intended-history/$(date +%Y.%m.%d-%H:%M:%S).csv
 mv current.csv last.csv
 git add .
-git commit -m "Automated commit by the run.sh script"
+git commit -m "Automated commit by the run.sh script at $(date +%Y.%m.%d-%H:%M:%S)"
 git push
 
